ee650ac8fb3104d5c4e40ae8d2cd263518ccf79b
Mary <mashynya18111992@gmail.com>
2022-12-13 23:07:07 +0300

    Ввести число, вывести от отрицательного до положительного

---
+++ Lesson1/Zadacha3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите положительное число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 < 0)
+{
+    num1 = num1 * -1;
+}
+else 
+{
+    Console.WriteLine("Введено число 0");
+}
+int num2 = -num1;
+
+while (num2 <= num1)
+{
+    Console.WriteLine(num2);
+    num2 = num2 + 1;
+}
