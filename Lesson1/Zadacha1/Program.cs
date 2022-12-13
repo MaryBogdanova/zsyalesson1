5f17586b55f8bc0aa40f28dc4b5a8eed568565c8
Mary <mashynya18111992@gmail.com>
2022-12-13 21:56:58 +0300

    первая задача

---
+++ Lesson1/Zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+    ((number2 * number2) == number1)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
